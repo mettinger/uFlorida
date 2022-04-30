68bde011b01d062a28e6433c070496ec12599a74
mettinger <jettinger35@gmail.com>
2022-04-30 15:17:02 -0400

    message

--- julia/saggioModel.jl
+++ julia/saggioModel.jl
@@ -3,22 +3,29 @@ plotlyjs()
 
 function parametersMake(z0)
 
-    A = [.3448, .0228, .2014]
-    B = [.3351, .0746, .2053]
-    c = .001
-    dstar = .3
+    flag = 1
+
+    if flag == 0
+        A = [.3448, .0228, .2014]
+        B = [.3351, .0746, .2053]
+        c = .001
+        dstar = .3
+        xs = .55
+    elseif flag == 1
+        A = [.2552,-.0637,.3014]
+        B = [.3496, .0795, .1774]
+        c = .001
+        dstar = .3
+        xs = .6
+    end
 
     R = norm(A)
     e = A/R
     crossProduct = cross(cross(A,B), A)
     f = crossProduct/norm(crossProduct)
 
-    #=
     muTwo, minusMuOne, nu = R * ((e * cos(z0)) + (f * sin(z0)))
     allRoots = roots(Polynomial([minusMuOne, -muTwo, 0, 1]))
-    xs = real(allRoots[1])
-    =#
-    xs = .55
 
     [e, f, R, c, dstar, xs]
 end
@@ -28,9 +35,6 @@ function saggio!(du, u, p, t)
 
     e, f, R, c, dstar, xs = p
     muTwo, minusMuOne, nu = R * ((e * cos(u[3])) + (f * sin(u[3])))
-
-    #allRoots = roots(Polynomial([minusMuOne, -muTwo, 0, 1]))
-    #xs = real(allRoots[1])
 
     du[1] = -u[2]
     du[2] = u[1]^3 - (muTwo * u[1]) + minusMuOne - (u[2] * (nu + u[1] + u[1]^2))
@@ -43,7 +47,7 @@ p = parametersMake(z0)
 x0 = p[6]
 
 u0 = [x0, y0, z0]
-tspan = (0., 2000.)
+tspan = (0., 6000.)
 
 prob = ODEProblem(saggio!, u0, tspan, p)
 sol = solve(prob)

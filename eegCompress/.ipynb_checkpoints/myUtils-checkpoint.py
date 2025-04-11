a344bc10694b53a77cc65d56c82651b882972a90
mettinger <jettinger35@gmail.com>
2025-04-11 18:44:04 -0400

    message

--- eegCompress/.ipynb_checkpoints/myUtils-checkpoint.py
+++ eegCompress/.ipynb_checkpoints/myUtils-checkpoint.py
@@ -6,7 +6,8 @@ timeZone = pytz.timezone('America/Los_Angeles')
 
 # print to console and also log file to deal with notebook disconnects
 def myPrint(stringToPrint):
-    print(stringToPrint)
     with open('/home/jmark.ettinger/github/uFlorida/eegCompress/notebookLog.txt', 'a') as file:
         timestamp = str(datetime.datetime.now().astimezone(timeZone).strftime('%m-%d %H:%M'))
-        file.write(timestamp + ": " + stringToPrint + "\n")
+        informationString = timestamp + ": " + stringToPrint + "\n"
+        print(informationString)
+        file.write(informationString)

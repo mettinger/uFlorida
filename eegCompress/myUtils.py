aa7a6fc72d3c514d9d0d3670c5921e36cc0df499
mettinger <jettinger35@gmail.com>
2025-04-12 16:35:13 -0400

    message

--- eegCompress/myUtils.py
+++ eegCompress/myUtils.py
@@ -7,7 +7,8 @@ timeZone = pytz.timezone('America/Los_Angeles')
 # print to console and also log file to deal with notebook disconnects
 def myPrint(stringToPrint):
     with open('/home/jmark.ettinger/github/uFlorida/eegCompress/notebookLog.txt', 'a') as file:
+        stringToPrint = str(stringToPrint)
         timestamp = str(datetime.datetime.now().astimezone(timeZone).strftime('%m-%d %H:%M'))
-        informationString = timestamp + ": " + stringToPrint + "\n"
+        informationString = timestamp + ": " + stringToPrint
         print(informationString)
-        file.write(informationString)
+        file.write(informationString + "\n")

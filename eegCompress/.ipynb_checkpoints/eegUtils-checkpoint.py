c66b7623ba83001aed7bdd18b7f1359741f7e97c
mettinger <jettinger35@gmail.com>
2025-04-16 15:58:52 -0400

    message

--- eegCompress/.ipynb_checkpoints/eegUtils-checkpoint.py
+++ eegCompress/.ipynb_checkpoints/eegUtils-checkpoint.py
@@ -8,55 +8,55 @@ import datetime
 import pytz
 timeZone = pytz.timezone('America/Los_Angeles')
 
+import torchModels
 from torchModels import *
 
 
 def predictEEG(model, interval, data, batch_size=128):
-  model.eval()
-
-  nChannel, nSample = data.shape
-  # if None predict the entire dataset
-  if interval == None:
-    start = model.numSampleInput
-    stop = nSample
-  else:
-    start, stop = interval
-
-  predicted = np.zeros((model.nChannel, stop - start))
-
-  dataTensorTruncated = torch.tensor(data[:,start - model.numSampleInput:stop])
-  if torch.cuda.is_available():
-    dataTensorTruncated = dataTensorTruncated.to('cuda')
-
-  dataset = datasetMake(dataTensorTruncated, model.numSampleInput, model.typeCode)
-  loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False, sampler=None)
-
-  with torch.no_grad():
-    i = 0
-    for modelInput, label in loader:
-        modelOutput = model(modelInput).detach().cpu().numpy().copy()
-        if modelOutput.ndim == 1:
-          predicted[:,i] = modelOutput
-          i += 1
-        else:
-          for j in range(modelOutput.shape[0]):
-            predicted[:,i] = modelOutput[j,:]
-            i += 1
-
-  if interval == None:
-    predicted = np.concatenate((data[:,0:model.numSampleInput], predicted), axis = 1)
-
-  model.train()
-  return predicted
+    model.eval()
+    
+    nChannel, nSample = data.shape
+    # if None predict the entire dataset
+    if interval == None:
+        start = model.numSampleInput
+        stop = nSample
+    else:
+        start, stop = interval
+        
+    predicted = np.zeros((model.nChannel, stop - start))
+    dataTensorTruncated = torch.tensor(data[:,start - model.numSampleInput:stop])
+    if torch.cuda.is_available():
+        dataTensorTruncated = dataTensorTruncated.to('cuda')
+        
+    dataset = torchModels.datasetMake(dataTensorTruncated, model.numSampleInput, model.typeCode)
+    loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False, sampler=None)
+    
+    with torch.no_grad():
+        i = 0
+        for modelInput, label in loader:
+            modelOutput = model(modelInput).detach().cpu().numpy().copy()
+            if modelOutput.ndim == 1:
+                predicted[:,i] = modelOutput
+                i += 1
+            else:
+                for j in range(modelOutput.shape[0]):
+                    predicted[:,i] = modelOutput[j,:]
+                    i += 1
+                    
+        if interval == None:
+            predicted = np.concatenate((data[:,0:model.numSampleInput], predicted), axis = 1)
+            
+    model.train()
+    return predicted
 
 def timeSeriesCompare(model, start, secondsToPlot, sFreq, data, numSampleInput, channel = 0, plotOption="both"):
     # currently only works for outputSamples = 1
 
     if secondsToPlot == None:
-      original = data
+        original = data
     else:
-      samplesToPlot = secondsToPlot * sFreq
-      original = data[:,start:start + samplesToPlot]
+        samplesToPlot = secondsToPlot * sFreq
+        original = data[:,start:start + samplesToPlot]
 
     predicted = predictEEG(model, (start, start + samplesToPlot), data)
     originalChannel = original[channel,:]
@@ -100,44 +100,41 @@ def saveModel(model, optimizer, epoch, loss, predicted):
     print("Model has been saved: " + saveName)
 
 def loadModel(path, model, optimizer, trainBool = True):
-
-  checkpoint = torch.load(path, weights_only=True)
-  model.load_state_dict(checkpoint['model_state_dict'])
-  optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-  epoch = checkpoint['epoch']
-  loss = checkpoint['loss']
-
-  model.eval()
-  if trainBool:
-    model.train()
-  else:
+    checkpoint = torch.load(path, weights_only=True)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch = checkpoint['epoch']
+    loss = checkpoint['loss']
     model.eval()
-
-  return model, optimizer, epoch, loss
+    if trainBool:
+        model.train()
+    else:
+        model.eval()
+        
+    return model, optimizer, epoch, loss
 
 
 def sizeToLayerList(layerSizeList):
-  layerList = []
-
-  for i in range(0, len(layerSizeList) - 1):
-      thisLayer = torch.nn.Linear(layerSizeList[i], layerSizeList[i + 1])
-      torch.nn.init.xavier_uniform_(thisLayer.weight)
-      layerList.append(thisLayer)
-      if i < len(layerSizeList) - 2:
-        layerList.append(torch.nn.LeakyReLU())
-
-  return layerList
+    layerList = []
+    for i in range(0, len(layerSizeList) - 1):
+        thisLayer = torch.nn.Linear(layerSizeList[i], layerSizeList[i + 1])
+        torch.nn.init.xavier_uniform_(thisLayer.weight)
+        layerList.append(thisLayer)
+        if i < len(layerSizeList) - 2:
+            layerList.append(torch.nn.LeakyReLU())
+            
+    return layerList
 
 def samplerMake(model, numSampleInput, data):
-  nChannel, nSample = data.shape
-  predicted = predictEEG(model, None, data)
-  residual = np.abs(data - predicted)
-  #residualMeasure = np.max(residual, axis=0)[numSampleInput:]
-  residualMeasure = np.mean(residual, axis=0)[numSampleInput:]
-  sampler = torch.utils.data.WeightedRandomSampler(weights=residualMeasure, num_samples=nSample)
-
-  print("Residual measure: " + str(np.max(residualMeasure)))
-  return sampler, predicted, residualMeasure
+    nChannel, nSample = data.shape
+    predicted = predictEEG(model, None, data)
+    residual = np.abs(data - predicted)
+    #residualMeasure = np.max(residual, axis=0)[numSampleInput:]
+    residualMeasure = np.mean(residual, axis=0)[numSampleInput:]
+    sampler = torch.utils.data.WeightedRandomSampler(weights=residualMeasure, num_samples=nSample)
+    
+    print("Residual measure: " + str(np.max(residualMeasure)))
+    return sampler, predicted, residualMeasure
 
 def modelSize(model):
     param_size = 0
